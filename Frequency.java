ae6c691866ba2264819cd6ab30aecb4b3256ed07
etseggai <e.d.tsegay@gmail.com>
2025-05-30 23:19:48 -0400

    Create Frequency.java

---
+++ Frequency.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Frequency extends Item{
+
+    private int frequency; 
+    private int index = 0;
+
+    public Frequency(String name, double price){
+        super(name, price);
+        this.frequency = 0;
+    }
+
+    public int setFrequency(ArrayList<Item> list, int size){
+        if (size < 0){
+            return frequency; 
+        }
+
+        if(((list.get(size)).getName()).equals(getName())){
+            frequency++;
+        }
+        size--;
+
+        return setFrequency(list, size);
+    }
+
+    public int getFrequency(){
+        return frequency;
+    }
+}
